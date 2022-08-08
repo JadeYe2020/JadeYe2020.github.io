8f3028316da6c2c5c5700e12d7503e930f7a3d36
Jade Ye <w0430394@nscc.ca>
2022-08-08 11:18:13 -0300

    creating dev branch to keep gobbledygook separate

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,31 @@
+import React, {useContext} from "react";
+import "./Footer.scss";
+import {Fade} from "react-reveal";
+import emoji from "react-easy-emoji";
+import StyleContext from "../../contexts/StyleContext";
+
+export default function Footer() {
+  const {isDark} = useContext(StyleContext);
+  return (
+    <Fade bottom duration={1000} distance="5px">
+      <div className="footer-div">
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          {emoji("Made with ❤️ by DeveloperFolio Team · Customized by Jade Ye")}
+        </p>
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          Theme by{" "}
+          <a href="https://github.com/saadpasta/developerFolio">
+            developerFolio
+          </a>
+        </p>
+        {/* jade: add attribution for new lottie landingPerson */}
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          landingPerson lottie by {" "}
+          <a href="https://lottiefiles.com/113856-vision">
+            Irfan Munawar 2D
+          </a>
+        </p>
+      </div>
+    </Fade>
+  );
+}
